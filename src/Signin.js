48825857d2c0433941007d6666326fffa2b8a47a
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-17 16:22:30 +0530

    1

--- src/Signin.js
+++ src/Signin.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import swal from 'sweetalert'
+import './Styles/signin.css';
+import main from './Images/MAIN.png'
 
 function Signin() {
   return (
     <div>
-        
+        <div className="form">
+          <div className='f-header'>
+            <center><p >SignIn</p></center>
+          </div>
+        </div>
+        <img src={main} className="main-img" alt=''/>
     </div>
   )
 }

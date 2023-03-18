abb6e2bdea53f130d8d245d35412cbb00b5c8616
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-18 13:03:58 +0530

    1

--- src/Signin.js
+++ src/Signin.js
@@ -6,64 +6,79 @@ import main from './Images/MAIN.png'
 import gicon from './Images/gicon.webp'
 import { useGoogleLogin } from '@react-oauth/google';
 import SuccessNavigate from './Components/SuccessNavigate';
+import Swal from 'sweetalert2'
 
 function Signin() {
-  
-  const [name , setName ] = useState();
-  const [email , setEmail ] = useState();
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
 
   const login = useGoogleLogin({
     onSuccess: tokenResponse => console.log(tokenResponse),
     onError: tokenResponse => console.log(tokenResponse),
-    onClick:tokenResponse => console.log(tokenResponse),
-    onNonOAuthError:tokenResponse =>alert(tokenResponse),
-    onChange:tokenResponse => console.log(tokenResponse),
-    
+    onClick: tokenResponse => console.log(tokenResponse),
+    onNonOAuthError: tokenResponse => alert(tokenResponse),
+    onChange: tokenResponse => console.log(tokenResponse),
+
   });
 
- 
+
 
   function fbFun() {
-
-    //🟡 FIREBASE DATA POSTING 
-    const fbUrl = 'https://todoapp-fb470-default-rtdb.firebaseio.com/' + name + '.json?'
-    fetch(fbUrl,
-    {
-        method: "PUT",
-        body: JSON.stringify({
+    // 🔴 ENTRY POINT CHECKING - NAME VALUE EMPTY OR NOT 
+    if (document.getElementById("name").value === "" && document.getElementById("email").value === "") {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+    } else {
+      // 🟡 FIREBASE DATA POSTING 
+      const fbUrl = 'https://todoapp-fb470-default-rtdb.firebaseio.com/' + name + '.json?'
+      fetch(fbUrl,
+        {
+          method: "PUT",
+          body: JSON.stringify({
             name,
-            email,        
+            email,
+          })
         })
-    })
-    localStorage.setItem("signin","1");
+      localStorage.setItem("signin", "1");
 
-    // ✅ AFTER ACCOUNT CREATING REDIRECTING TO HOME PAGE
+      // ✅ AFTER ACCOUNT CREATING REDIRECTING TO HOME PAGE
 
-    <SuccessNavigate/>
-    // if(localStorage.getItem("signin") === "1" ){
-    //   <SuccessNavigate/>
-    // }
-}
+
+      if (localStorage.getItem("signin") === "1") {
+        window.location.replace('http://localhost:3000/home');
+      }
+      else {
+        alert("login faled")
+      }
+    }
+
+
+  }
 
   return (
     <div>
-        {/* MANUAL LOGIN FORM */}
-        <div className="form">
-          <div className='f-header'>
-            <center><p className='f-title'>⎯  Sign In ⎯</p></center>
-            <center><p className='f-des'>Create account to access full features</p></center>
-            <center><input o placeholder='Name' className='name' type="text" value={name} required onChange={e => setName(e.target.value)}/></center>
-            <center><input placeholder='email'  className='email' type="email" required value={email} onChange={e => setEmail(e.target.value)}/></center>
-            <center><input onClick={fbFun} className='signin'> Sign In</input></center>
-          </div>
+      {/* MANUAL LOGIN FORM */}
+      <div className="form">
+        <div className='f-header'>
+          <center><p className='f-title'>⎯  Sign In ⎯</p></center>
+          <center><p className='f-des'>Create account to access full features</p></center>
+          <center><input placeholder='Name' id='name' className='name' type="text" value={name} required onChange={e => setName(e.target.value)} /></center>
+          <center><input placeholder='email' id='email' className='email' type="email" required value={email} onChange={e => setEmail(e.target.value)} /></center>
+          <center><button onClick={fbFun} className='signin' > Sign In</button></center>
         </div>
+      </div>
 
       {/* GOOGLE LOGIN FORM */}
       <div onClick={login} className='gform'>
-      <img src={gicon} height="40px" width="40px" className="gicon" alt=''/>
-      <p id='gtext'>Continue with Google</p>
-     </div>
-        <img src={main} className="main-img" alt=''/>
+        <img src={gicon} height="40px" width="40px" className="gicon" alt='' />
+        <p id='gtext'>Continue with Google</p>
+      </div>
+      <img src={main} className="main-img" alt='' />
     </div>
   )
 }

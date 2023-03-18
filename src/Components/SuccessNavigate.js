1008bc9d1906d26b26c6a9032abfac015aeadcf4
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-18 14:19:57 +0530

    1

--- src/Components/SuccessNavigate.js
+++ src/Components/SuccessNavigate.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../Styles/signinSuccess.css'
+import successAnimation from '../Images/success.mp4'
+import { Player } from 'video-react';
+
 
 function SuccessNavigate() {
 
   return (
-    <div></div>
+    <div>
+        <Player
+      playsInline
+      poster="/assets/poster.png"
+      src={successAnimation}
+    />
+       
+    </div>
   )
 }
 

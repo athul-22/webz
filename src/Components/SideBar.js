ab0fd2ebdb097eac4c576f3adf17fa6f348e21bc
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-19 12:32:02 +0530

    1

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,23 +1,17 @@
+import { Sidebar, setVisibleCustomToolbar, visibleCustomToolbar , Button} from 'primereact/sidebar';
 import React from 'react';
-import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
 
-function Layout() {
-  const { collapseSidebar } = useProSidebar();
-
-  return (
-    <div style={{ display: 'flex', height: '100%' }}>
-      <Sidebar>
-        <Menu>
-          <MenuItem> Documentation</MenuItem>
-          <MenuItem> Calendar</MenuItem>
-          <MenuItem> E-commerce</MenuItem>
-        </Menu>
-      </Sidebar>
-      <main>
-        <button onClick={() => collapseSidebar()}>Collapse</button>
-      </main>
-    </div>
-  );
-}
-
-export default Layout;
+<Sidebar visible={visibleCustomToolbar} onHide={() => setVisibleCustomToolbar(false)} icons={() => (
+    <React.Fragment>
+        <button className="p-sidebar-close p-link">
+            <span className="p-sidebar-close-icon pi pi-print"/>
+        </button>
+        <button className="p-sidebar-close p-link">
+            <span className="p-sidebar-close-icon pi pi-arrow-right"/>
+        </button>
+    </React.Fragment>
+)}>
+    <h1 style={{fontWeight:'normal'}}>Sidebar with custom icons</h1>
+    <Button type="button" onClick={(e) => setVisibleCustomToolbar(false)} label="Save" className="p-button-success" style={{marginRight:'.25em'}} />
+    <Button type="button" onClick={(e) => setVisibleCustomToolbar(false)} label="Cancel" className="p-button-secondary"/>
+</Sidebar>

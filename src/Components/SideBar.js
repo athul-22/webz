6a479d531bf1c36a2b2108d63f6300a11c527ab0
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-19 12:38:04 +0530

    2

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,17 +1,60 @@
-import { Sidebar, setVisibleCustomToolbar, visibleCustomToolbar , Button} from 'primereact/sidebar';
-import React from 'react';
 
-<Sidebar visible={visibleCustomToolbar} onHide={() => setVisibleCustomToolbar(false)} icons={() => (
-    <React.Fragment>
-        <button className="p-sidebar-close p-link">
-            <span className="p-sidebar-close-icon pi pi-print"/>
-        </button>
-        <button className="p-sidebar-close p-link">
-            <span className="p-sidebar-close-icon pi pi-arrow-right"/>
-        </button>
-    </React.Fragment>
-)}>
-    <h1 style={{fontWeight:'normal'}}>Sidebar with custom icons</h1>
-    <Button type="button" onClick={(e) => setVisibleCustomToolbar(false)} label="Save" className="p-button-success" style={{marginRight:'.25em'}} />
-    <Button type="button" onClick={(e) => setVisibleCustomToolbar(false)} label="Cancel" className="p-button-secondary"/>
-</Sidebar>
+import React, { useState } from 'react';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
+
+const SidebarDemo = () => {
+  const [visibleLeft, setVisibleLeft] = useState(false);
+  const [visibleRight, setVisibleRight] = useState(false);
+  const [visibleTop, setVisibleTop] = useState(false);
+  const [visibleBottom, setVisibleBottom] = useState(false);
+  const [visibleFullScreen, setVisibleFullScreen] = useState(false);
+  const [visibleCustomToolbar, setVisibleCustomToolbar] = useState(false);
+
+  const customIcons = (
+      <React.Fragment>
+          <button className="p-sidebar-icon p-link mr-1">
+              <span className="pi pi-print" />
+          </button>
+          <button className="p-sidebar-icon p-link mr-1">
+              <span className="pi pi-arrow-right" />
+          </button>
+      </React.Fragment>
+  );
+
+  return (
+      <div>
+          <div className="card">
+              <Sidebar visible={visibleLeft} onHide={() => setVisibleLeft(false)}>
+                  <h3>Left Sidebar</h3>
+              </Sidebar>
+
+              <Sidebar visible={visibleRight} position="right" onHide={() => setVisibleRight(false)}>
+                  <h3>Right Sidebar</h3>
+              </Sidebar>
+
+              <Sidebar visible={visibleTop} position="top" onHide={() => setVisibleTop(false)}>
+                  <h3>Top Sidebar</h3>
+              </Sidebar>
+
+              <Sidebar visible={visibleBottom} position="bottom" onHide={() => setVisibleBottom(false)} modal={false} dismissable>
+                  <h3>Bottom Sidebar</h3>
+              </Sidebar>
+
+              <Sidebar visible={visibleFullScreen} fullScreen onHide={() => setVisibleFullScreen(false)}>
+                  <h3>Full Screen Sidebar</h3>
+              </Sidebar>
+
+              <Sidebar visible={visibleCustomToolbar} onHide={() => setVisibleCustomToolbar(false)} icons={customIcons}>
+                  <h3>Sidebar with custom icons</h3>
+              </Sidebar>
+
+              <Button icon="pi pi-arrow-right" onClick={() => setVisibleLeft(true)} className="mr-2" />
+          </div>
+      </div>
+  )
+}          
+
+
+export default SidebarDemo;

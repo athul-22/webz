13cc2152b9dabb123b279c4eb97dfbb5f4b55c24
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-20 08:40:32 +0530

    1

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,27 +1,8 @@
 import React from "react";
 import '../Styles/sidebar.css'
-import { $ }  from 'react-jquery-plugin'
 
 function Layout() {
 
-    const sidebarToggle = document.querySelector(".sidebar__toggle"),
-        sidebar = document.querySelector(".sidebar"),
-        sidebarLink = document.querySelectorAll(".sidebar__link");
-
-    sidebarToggle.addEventListener("click", () => {
-        sidebar.classList.toggle("sidebar--open");
-    });
-
-    function activeLink() {
-        sidebarLink.forEach((link) => {
-            link.classList.remove("sidebar__link--active");
-            this.classList.add("sidebar__link--active");
-        });
-    }
-
-    sidebarLink.forEach((link) => {
-        link.addEventListener("click", activeLink);
-    });
 
     return (
         <div style={{ display: 'flex', height: '100%', minHeight: '400px' }}>
@@ -124,7 +105,11 @@ function Layout() {
                     <h1>Sidebar</h1>
                 </div>
             </main>
+            <script>
+            
+        </script>
         </div>
+        
     );
 }
 

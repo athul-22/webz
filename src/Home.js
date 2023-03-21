aaafd4be08874276b34e4afd626f2a2e28be02f8
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-21 18:33:44 +0530

    1

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,59 @@
 import React from 'react'
 import "./Styles/home.css"
 import SideBar from './Components/SideBar'
+import './Styles/sidebar.css';
+import dashboard from './Images/dashboard.png'
+import user from './Images/user.png'
+import fire from './Images/fire.png'
+import profile from './Images/profile.png'
+import $ from 'jquery';
 
 function Home() {
 
-  let dashboard = document.getElementsByClassName("dashboard");
+  let home = document.getElementsByClassName("dashboard");
   let trending = document.getElementsByClassName("trending");
   let account = document.getElementsByClassName("account");
 
+  function dashFun(){
+    $(".dashboard").css('display') = "none"
+  }
+
   return (
+    
     <div>
-      {/* ✅ SIDEBAR */}
-      <SideBar />
+        {/* ✅ SIDEBAR STARTING */}
+        <div className="sidenav">
+                <div className="user">
+                  <center><img className="profile" src={profile} height="60px" width="60px" alt=""/><br/></center> 
+                    <center><p className="nam">Athul Nambiar</p></center>
+                    <center><p className="mail">adbit.tesa@gmail.com</p></center>
+                </div>
+               
+                <ul>
+                    <li><img height="50px" width="50px" alt="" src={dashboard}/><p>Dashboard</p></li>
+                    <li><img height="50px" width="50px" alt="" src={fire}/><p>Trending</p></li>
+                    <li><img height="50px" width="50px" alt="" src={user}/><p>Account</p></li>
+                </ul>
+
+            {/* ✅ LOGOUT - BOTTOM NAV */}
+                <ul>
+                    <li>Logout</li>
+                </ul>
+            </div>
      
+    {/* ✅ DASHBOARD */} 
     <div className='dashboard'>
-      <h1>dashboard</h1>
+      <h1>DASHBOARD</h1>
     </div>
 
+    {/* ✅ TRENDING */} 
     <div className='trending'>
-      <h1>dashboard</h1>
+      <h1>TRENDING</h1>
     </div>
 
+    {/* ✅ ACCOUNT */} 
     <div className='account'>
-      <h1>dashboard</h1>
+      <h1>ACCOUTN</h1>
     </div>
 
     </div>

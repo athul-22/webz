c06d5b4ff81f444440c8e8672cdfaf39eaf20fdd
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-21 19:54:48 +0530

    1

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import "./Styles/home.css"
-import SideBar from './Components/SideBar'
 import './Styles/sidebar.css';
 import dashboard from './Images/dashboard.png'
 import user from './Images/user.png'
 import fire from './Images/fire.png'
 import profile from './Images/profile.png'
+import notification from './Images/notification.png'
 import $ from 'jquery';
+import Notifications from "react-notifications-menu";
+
 
 function Home() {
+
+  const DEFAULT_NOTIFICATION = {
+    image:
+      "https://cutshort-data.s3.amazonaws.com/cloudfront/public/companies/5809d1d8af3059ed5b346ed1/logo-1615367026425-logo-v6.png",
+    message: "Notification one.",
+    detailPage: "/events",
+    receivedTime: "12h ago"
+  };
+  
 
   let home = document.getElementsByClassName("dashboard");
   let trending = document.getElementsByClassName("trending");
@@ -57,7 +68,18 @@ function Home() {
      
     {/* ✅ DASHBOARD */} 
     <div className='dashboard'>
-      <p id='header'>| Dashbaord</p>
+     
+     <div className='dash_top'>
+     <p id='header'>| Dashbaord</p>
+     <img src={notification} alt="" className="notification"/>
+     <Notifications
+          
+          header={{
+            title: "Notifications",
+            option: { text: "View All", onClick: () => console.log("Clicked") }
+          }}/>
+     </div>
+
     </div>
 
     {/* ✅ TRENDING */} 

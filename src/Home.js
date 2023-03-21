d6d40b7ff6b2481458a27c074407c96fcb444d50
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-21 17:12:20 +0530

    1

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,13 @@ import "./Styles/home.css"
 import SideBar from './Components/SideBar'
 
 function Home() {
-  
+
   return (
     <div>
-     <SideBar/>
-        <h1 id="h">home</h1>
-        </div>
+      {/* ✅ SIDEBAR */}
+      <SideBar />
+      <iframe src='https://www.adbit.in' className='frame' height="100vh" width="100%" title='s'>Not supported</iframe>
+    </div>
   )
 }
 

0d7507ed199be5db7c7264da132c96ea3e0fe186
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-22 20:38:18 +0530

    1

--- src/Instagram.js
+++ src/Instagram.js
@@ -30,7 +30,7 @@ function Instagram() {
     return (
         <div>
             <button onClick={igFun}> click me</button>
-            <img id='profile'  src="https://instagram.flhr13-1.fna.fbcdn.net/v/t51.2885-19/274250588_1101859380610497_3463460270900219221_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.flhr13-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=aPy5rZOCjScAX8Ih901&edm=AJfeSrwBAAAA&ccb=7-5&oh=00_AfAUcniWrrZ8AQAPJgt0n3vtP1_CEXttd6D6GF6K8rMb-g&oe=641F45FA&_nc_sid=588073" alt="" />
+            <img id='profile' height="100px" width="100px" src={imgUrl} alt="" />
         </div>
     )
 }

012b30893df2e8451e4253f638d9172ab8fc9cec
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-23 21:51:55 +0530

    1

--- src/Instagram.js
+++ src/Instagram.js
@@ -53,7 +53,9 @@ function Instagram() {
         // Get a data URL string for the canvas
         const dataURL = canvas.toDataURL();
 
-       
+        console.log("BELOW LOOK")
+        console.log(dataURL);
+
 
 
     }

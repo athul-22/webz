f5df448b979eb33c5efb4fd3ae8f6b8296deed8a
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-26 17:43:21 +0530

    1

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,11 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://athul:athul@athul.zzuss6q.mongodb.net/athul';
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("athul").collection("users");
   // perform actions on the collection object
   client.close();
 });
+

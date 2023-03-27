6467caf8ac061e20835f5e68db88c62dc417ba03
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-27 12:39:27 +0530

    1

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ const uri = 'mongodb+srv://athul:athul@athul.zzuss6q.mongodb.net/athul';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
+  
   const collection = client.db("athul").collection("users");
   // perform actions on the collection object
   client.close();

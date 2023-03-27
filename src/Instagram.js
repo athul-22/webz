fd5c431914df3a26f99494baab56c3fbfe942d6f
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-27 13:03:25 +0530

    1

--- src/Instagram.js
+++ src/Instagram.js
@@ -1,21 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { collection } from './db';
+import React, { useState } from 'react';
+import { MongoClient } from './db';
 
-function Instagram() {
+function App() {
+  const [inputValue, setInputValue] = useState('');
 
-  const [data, setData] = useState([]);
+  const handleClick = async () => {
+    const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+    try {
+      await client.connect();
+      const database = client.db('myDatabase');
+      const collection = database.collection('myCollection');
+      const result = await collection.insertOne({ data: inputValue });
+      console.log(`Inserted ${result.insertedCount} documents into the collection`);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await client.close();
+    }
+  };
 
-  
   return (
     <div>
-      {data.map(item => (
-        <div key={item._id}>
-          <h2>{item.title}</h2>
-          <p>{item.content}</p>
-        </div>
-      ))}
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
 
-export default Instagram;
+export default App;

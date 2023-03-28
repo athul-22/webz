8b0aef841d0764426eabf93b5d1d691d22eb9e3f
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-28 07:18:50 +0530

    1

--- src/Instagram.js
+++ src/Instagram.js
@@ -1,50 +1,50 @@
-import './App.css';
-import React, { useState } from 'react';
-import Axios from 'axios';
-import './Styles/instagram.css'
+// import './App.css';
+// import React, { useState } from 'react';
+// import Axios from 'axios';
+// import './Styles/instagram.css'
 
-function Instagram() {
-  const [name, setName] = useState("")
-  const [role, setRole] = useState("")
+// function Instagram() {
+//   const [name, setName] = useState("")
+//   const [role, setRole] = useState("")
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
+//   const handleSubmit = (e) => {
+//       e.preventDefault();
 
-      Axios.post('http://localhost:4000/insert', {
-          fullName: name,
-          companyRole:role
-      })
-  }
+//       Axios.post('http://localhost:3000/insert', {
+//           fullName: name,
+//           companyRole:role
+//       })
+//   }
 
-  return (
-    <div className="App">
-      <header className="App-header"> 
-        <div className="logIn-form">
-            <form onSubmit={handleSubmit}>
-                <p>First Name</p>
+//   return (
+//     <div className="App">
+//       <header className="App-header"> 
+//         <div className="logIn-form">
+//             <form onSubmit={handleSubmit}>
+//                 <p>First Name</p>
 
-                <input
-                  className = "Name" 
-                  type="text"
-                  placeholder="First name ..."
-                  onChange={(e) => {setName(e.target.value)}}
-                />
+//                 <input
+//                   className = "Name" 
+//                   type="text"
+//                   placeholder="First name ..."
+//                   onChange={(e) => {setName(e.target.value)}}
+//                 />
 
-                <p> Company Role</p>
+//                 <p> Company Role</p>
 
-                <input 
-                  className = "Role"
-                  type="text"
-                  placeholder = "Role...." 
-                  onChange={(e) => {setRole(e.target.value)}}
-                />
+//                 <input 
+//                   className = "Role"
+//                   type="text"
+//                   placeholder = "Role...." 
+//                   onChange={(e) => {setRole(e.target.value)}}
+//                 />
 
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-      </header>
-    </div>
-  );
-}
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default Instagram
+// export default Instagram
--- src/server/dataSchema.js
+++ src/server/dataSchema.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const ReactFormDataSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    }
-});
+// const ReactFormDataSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     role: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-const User = mongoose.model('User', ReactFormDataSchema);
-module.exports = User;
+// const User = mongoose.model('User', ReactFormDataSchema);
+// module.exports = User;

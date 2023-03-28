8a81fb021b3d0e106e964ec12e6c7a1d300380c1
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-28 23:14:09 +0530

    1

--- src/Components/db.js
+++ src/Components/db.js
@@ -3,7 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  
+    apiKey: "AIzaSyAl9sbbgwxkyeh1rfQMVxbPnvNvX6SlS4s",
+    authDomain: "todoapp-fb470.firebaseapp.com",
+    databaseURL: "https://todoapp-fb470-default-rtdb.firebaseio.com",
+    projectId: "todoapp-fb470",
+    storageBucket: "todoapp-fb470.appspot.com",
+    messagingSenderId: "453121987629",
+    appId: "1:453121987629:web:923cc5722fa474bef314a4"
 };
 
 if (!firebase.apps.length) {

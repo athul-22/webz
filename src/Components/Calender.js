2e0f4421fb2fe02ac5297afa6f97411a425e549f
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2023-04-10 17:32:22 +0000

    Update Calender.js

--- src/Components/Calender.js
+++ src/Components/Calender.js
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 function Calender() {
+    
     const [value, onChange] = useState(new Date());
   
     return (

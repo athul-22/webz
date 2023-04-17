66f1efb53d04afc5e600259fb68fd362ca0f9d11
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2023-04-17 23:35:03 +0530

    Update Calender.js

--- src/Components/Calender.js
+++ src/Components/Calender.js
@@ -6,7 +6,6 @@ function Calender() {
     const [value, onChange] = useState(new Date());
     return (
       <div>
-        
         <Calendar onChange={onChange} value={value} />
       </div>
     );
